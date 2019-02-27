f43ba9a66f75cb90e184aefde911d22cf4283ccb
mmheydari97 <heidarymm@yahoo.com>
2019-02-28 02:40:56 +0330

    accounts done

--- simplesocial/accounts/models.py
+++ simplesocial/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import User as AuthUser
+
 
 # Create your models here.
+class User(AuthUser, PermissionsMixin):
+
+    def __str__(self):
+        return "@{}".format(self.username)
--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts.apps.AccountsConfig',
 ]
 
@@ -122,3 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static'), ]
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
