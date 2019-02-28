ac80e63e317611aa861ccb5b0c3a9f350b02ff72
mmheydari97 <heidarymm@yahoo.com>
2019-02-28 20:04:22 +0330

    before debug

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-    'accounts.apps.AccountsConfig',
+    'accounts',
+    'posts',
+    'groups',
 ]
 
 MIDDLEWARE = [
--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path, include
-from simplesocial import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.HomePage.as_view(), name='home'),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('test/', views.TestPage.as_view(), name='test'),
-    path('thanks/', views.ThanksPage.as_view(), name='thanks'),
-
+    url(r"^$", views.HomePage.as_view(), name="home"),
+    url(r"^test/$", views.TestPage.as_view(), name="test"),
+    url(r"^thanks/$", views.ThanksPage.as_view(), name="thanks"),
+    url(r"^admin/", admin.site.urls),
+    url(r"^accounts/", include("accounts.urls", namespace="accounts")),
+    url(r"^accounts/", include("django.contrib.auth.urls")),
+    url(r"^posts/", include("posts.urls", namespace="posts")),
+    url(r"^groups/", include("groups.urls", namespace="groups")),
 ]
